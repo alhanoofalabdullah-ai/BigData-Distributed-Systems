d55a6536b4bf8e481a6a528afdd855a36625d613
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-09 17:06:21 +0300

    Create log_consumer.py

---
+++ project-05-real-time-log-processing-platform/log_consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+
+    "application-logs",
+
+    bootstrap_servers="localhost:9092",
+
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+
+for message in consumer:
+
+    print(message.value)
