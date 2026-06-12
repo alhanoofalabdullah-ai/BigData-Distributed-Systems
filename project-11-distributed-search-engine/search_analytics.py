9fce788033cd40dc60fcbb4e54047d2ee684e673
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 08:51:08 +0300

    Create search_analytics.py

---
+++ project-11-distributed-search-engine/search_analytics.py
@@ -0,0 +1,13 @@
+search_metrics = {
+
+    "total_searches": 25000,
+
+    "average_response_time_ms": 45,
+
+    "successful_queries": 24800,
+
+    "failed_queries": 200
+
+}
+
+print(search_metrics)
