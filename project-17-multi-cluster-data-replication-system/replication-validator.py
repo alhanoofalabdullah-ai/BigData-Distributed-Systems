30883d99d4fe49ecdc2cfb986679f3d5be82301a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 10:27:27 +0300

    Create replication-validator.py

---
+++ project-17-multi-cluster-data-replication-system/replication-validator.py
@@ -0,0 +1,11 @@
+source_records = 100000
+
+target_records = 100000
+
+if source_records == target_records:
+
+    print("Replication Validation Passed")
+
+else:
+
+    print("Replication Validation Failed")
