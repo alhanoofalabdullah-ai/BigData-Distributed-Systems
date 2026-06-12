f8d7816a8b24f20cde0029beb6d6db0ebe4676e8
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 10:36:45 +0300

    Create model_evaluation.py

---
+++ project-18-distributed-machine-learning-pipeline/model_evaluation.py
@@ -0,0 +1,15 @@
+accuracy = 94.5
+
+precision = 92.1
+
+recall = 91.8
+
+f1_score = 91.9
+
+print("Accuracy:", accuracy)
+
+print("Precision:", precision)
+
+print("Recall:", recall)
+
+print("F1 Score:", f1_score)
